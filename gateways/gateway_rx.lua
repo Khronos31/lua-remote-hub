d203babe0acda099b64171092bef5845e34e841f
Khronos31 <39792509+Khronos31@users.noreply.github.com>
2026-03-01 19:14:27 +0900

    Update to HA Addon

---
+++ gateways/gateway_rx.lua
@@ -0,0 +1,19 @@
+local usbir = require("usbir")
+local socket = require("socket.http")
+local ltn12 = require("ltn12")
+local cjson = require("cjson")
+
+local rdev = assert(usbir.open(0))
+local HA_URL = "http://homeassistant.local:8123/api/webhook/ir_remote_gateway"
+
+while true do
+    local data = rdev:receive()
+    if data then
+        local hex = (data:gsub('.', function(c) return string.format('%02X', string.byte(c)) end))
+        socket.request {
+            url = HA_URL, method = "POST",
+            headers = { ["Content-Type"] = "application/json" },
+            source = ltn12.source.string(cjson.encode({ code = hex }))
+        }
+    end
+end
