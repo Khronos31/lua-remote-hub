3f0ae462ad7cfb8c401d400046f66252a0eb8290
Khronos31 <39792509+Khronos31@users.noreply.github.com>
2026-03-07 03:34:31 +0900

    Refactor config to use structured mode definitions

--- ha-addon/config/config.lua
+++ ha-addon/config/config.lua
@@ -1,21 +1,17 @@
 local lrh      = require("lrh_util")
-local C_RT1    = require("C-RT1")
-local J_MX     = require("J-MX100RC")
-local RC_R2    = require("RC-R2")
-local Nintendo = require("Bluetooth")
-local HDMI     = require("HDMI-Switcher")
-local HDMI_CEC = require("HDMI-CEC")
 
 local config = {}
-local mode_tv       = {}
-local mode_recorder = {}
-local mode_switch   = {}
+config.C_RT1    = require("C-RT1")
+config.J_MX     = require("J-MX100RC")
+config.RC_R2    = require("RC-R2")
+config.Nintendo = require("Bluetooth")
+config.HDMI     = require("HDMI-Switcher")
+config.HDMI_CEC = require("HDMI-CEC")
 
--- モード名の文字列とテーブルの実体を紐付け
 config.modes = {
-  ["TV"]       = mode_tv,
-  ["Recorder"] = mode_recorder,
-  ["Switch"]   = mode_switch
+  ["TV"]       = {},
+  ["Recorder"] = {},
+  ["Switch"]   = {}
 }
 
 -- 初期モードを設定
@@ -24,43 +20,43 @@ config.current_mode = config.modes["TV"]
 config.gateway_tx_url = "http://192.168.1.142:8080"
 
 -- 1. 基本一括バインド
-lrh.bind(mode_tv,       C_RT1, J_MX)
-lrh.bind(mode_recorder, C_RT1, RC_R2)
-lrh.bind(mode_switch,   C_RT1, Nintendo)
+lrh.bind(config.modes.TV,       config.C_RT1, config.J_MX)
+lrh.bind(config.modes.Recorder, config.C_RT1, config.RC_R2)
+lrh.bind(config.modes.Switch,   config.C_RT1, config.Nintendo)
 
 -- 2. 数字キーの放送波集約（D, BS, CS, A すべてを各デバイスのNUMに紐付け）
 local prefixes = { "D_", "BS_", "CS_", "A_" }
 for i = 1, 12 do
   for _, pre in ipairs(prefixes) do
-    local source_sig = C_RT1.keys[pre .. i]
-    mode_tv[source_sig]       = { type = "IR", code = J_MX.keys["NUM_"..i] }
-    mode_recorder[source_sig] = { type = "IR", code = RC_R2.keys["NUM_"..i] }
+    local source_sig = config.C_RT1.keys[pre .. i]
+    config.TV[source_sig]       = { type = "IR", code = config.J_MX.keys["NUM_"..i] }
+    config.Recorder[source_sig] = { type = "IR", code = config.RC_R2.keys["NUM_"..i] }
   end
 end
 
 -- 3. メイン設定（共通操作 & モード切替）
 config.remap = {
   -- モード切替
-  [C_RT1.keys.SUB_CH] = function()
+  [config.C_RT1.keys.SUB_CH] = function()
     lrh.set_mode_to_ha("TV")
-    lrh.send_ir(J_MX.keys.MODE_DIGITAL)
+    lrh.send_ir(config.J_MX.keys.MODE_DIGITAL)
     print("📺 Mode: TV")
   end,
   [C_RT1.keys.WOOO_LINK] = function()
     lrh.set_mode_to_ha("Recorder")
-    lrh.send_cec(HDMI_CEC.keys.HDMI_1)
-    lrh.send_ir(HDMI.keys.NUM_3)
+    lrh.send_cec(config.HDMI_CEC.keys.HDMI_1)
+    lrh.send_ir(config.HDMI.keys.NUM_3)
     print("📼 Mode: Recorder")
   end,
   [C_RT1.keys.INTERNET] = function()
     lrh.set_mode_to_ha("Switch")
-    lrh.send_cec(HDMI_CEC.keys.HDMI_1)
-    lrh.send_ir(HDMI.keys.NUM_2)
+    lrh.send_cec(config.HDMI_CEC.keys.HDMI_1)
+    lrh.send_ir(config.HDMI.keys.NUM_2)
     print("🎮 Mode: Switch")
   end,
 
   -- 全モード共通
-  [C_RT1.keys.INPUT_SELECT] = { type = "IR", code = J_MX.keys.INPUT_SELECT },
+  [config.C_RT1.keys.INPUT_SELECT] = { type = "IR", code = config.J_MX.keys.INPUT_SELECT },
 }
 
 return config
