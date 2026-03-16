87aacd706c9194369142f25b3fc5a98122806056
Khronos31 <39792509+Khronos31@users.noreply.github.com>
2026-03-16 14:05:59 +0900

    run.sh の更新

--- ha-addon/run.sh
+++ ha-addon/run.sh
@@ -2,5 +2,18 @@
 
 echo "🚀 LRH Logic Controller Starting..."
 
+# ユーザー側のディレクトリ（/config は addon_config:rw でマウントされている前提）
+# にファイルが一つもなければ、初期サンプルをコピーする
+if [ ! -f "/config/config.lua" ]; then
+    bashio::log.info "Config files not found. Initializing with default configs..."
+    cp -pr /defaults/. /config/
+else
+    bashio::log.info "Config files found. Skipping initialization."
+fi
+
+# その後、本来の起動処理へ
+# 例: lua /ha-addon/main.lua
+
+
 # Lua 5.4 でメインスクリプトを実行
 lua5.4 /main.lua
